6cabe13784bb6c019e442eda6f377041d64b6817
mariogsc12 <m.gomez03016@gmail.com>
2025-01-19 11:57:41 +0100

    radio communication - receiver and transmitter

---
+++ radio_communication/radio_receiver/src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include <RF24.h>
+
+// NRF24L01 Pinout
+#define CE 9
+#define CSN 10
+
+RF24 radio(CE, CSN);
+
+const byte canalId[6] = "00001"; // Communication channel
+
+void setup()
+{
+    Serial.begin(115200); // Debugging output
+    radio.begin();
+    radio.openReadingPipe(1, canalId);
+    radio.startListening();
+}
+
+void loop()
+{
+    char receivedBuffer[32]; // Buffer to store the received data
+    if (radio.available())
+    {
+        radio.read(receivedBuffer, sizeof(receivedBuffer));
+
+        // Debugging output
+        Serial.print("Received raw: ");
+        Serial.print(receivedBuffer);
+
+        // Parse the received string
+        int receivedData[3];
+        sscanf(receivedBuffer, "%d,%d,%d", &receivedData[0], &receivedData[1], &receivedData[2]);
+
+        // Print the received data
+        Serial.print("- Parsed: ");
+        Serial.print(receivedData[0]);
+        Serial.print(", ");
+        Serial.print(receivedData[1]);
+        Serial.print(", ");
+        Serial.println(receivedData[2]);
+    }
+}
