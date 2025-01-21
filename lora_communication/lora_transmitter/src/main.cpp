8a159f0e17c19d616c4f72982e4fec40e90ab14e
mariogsc12 <m.gomez03016@gmail.com>
2025-01-21 20:52:34 +0100

    create LoRa class

--- lora_communication/lora_transmitter/src/main.cpp
+++ lora_communication/lora_transmitter/src/main.cpp
@@ -8,13 +8,14 @@ const int csPin = 4;          // LoRa radio chip select
 const int resetPin = 2;       // LoRa radio reset
 const int irqPin = 15;        // Change for your board; must be a hardware interrupt pin
 
-int g_msg2 = 20;
-int g_msg3 = 30;
-int g_msg4 = 40;
-int g_msg5 = 50;
-int g_msg6 = 60;
-int g_msg7 = 70;
-int g_msg8 = 80;
+float g_msg2 = 50.0;  // battery
+int g_msg3 = 20;      // commandThrottle
+int g_msg4 = -20;     // commandSteering
+int g_msg5 = 1;       // RcMode
+float g_msg6 = 0.5;   // pitch
+float g_msg7 = 5.5;   // yaw
+int g_msg8 = 80;      // left_motor
+int g_msg9 = -80;     // right_motor
 
 void setup() {
   Serial.begin(115200); // Initialize serial communication
@@ -34,9 +35,9 @@ void sendLoRaMessage() {
   unsigned long currentMillis = millis(); // Get the time in milliseconds since the program started
 
   // Create a message with the current time and other values separated by commas
-  String message = String(currentMillis) + "," + String(g_msg2) + "," + String(g_msg3) + "," +
+  String message = String(currentMillis/1000) + "," + String(g_msg2) + "," + String(g_msg3) + "," +
                    String(g_msg4) + "," + String(g_msg5) + "," + String(g_msg6) + "," +
-                   String(g_msg7) + "," + String(g_msg8);
+                   String(g_msg7) + "," + String(g_msg8) + "," + String(g_msg9);
 
   LoRa.beginPacket();       // Start a new LoRa packet
   LoRa.print(message);      // Add the message to the packet
@@ -50,8 +51,8 @@ void loop() {
   static unsigned long lastSendTime = 0;
   unsigned long currentTime = millis();
 
-  // Send a message every 1000 milliseconds (1 second)
-  if (currentTime - lastSendTime >= 1000) {
+  // Send a message every 500 milliseconds (1 second)
+  if (currentTime - lastSendTime >= 500) {
     lastSendTime = currentTime;
     sendLoRaMessage();
   }
