49b65bafd4b8038964f991473b8a48a6e9326797
mariogsc12 <m.gomez03016@gmail.com>
2025-04-02 22:40:56 +0200

    MPU9250 wrapper
    - Use complementary filter to calculate angles (pending to check)
    - Compute orientation values based on trigonometry

---
+++ Software/MPU9250/src/Imu.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+
+#include <MPU9250_WE.h>
+#include <Wire.h>
+#include <Vector.h>
+
+#define MPU9250_ADDR 0x68
+
+extern MPU9250_WE mpu;         //= MPU9250_WE(MPU9250_ADDR);
+
+class Imu{
+    private:
+        Vector3D <float> accRaw;
+        Vector3D <float> gyroRaw;
+        Vector3D <float> gyroOffset;
+        
+        Vector3D <float> angle;
+        Vector3D <float> accOrientation;
+
+        // Other variables used to store temporal data
+        float dt;
+        long prev_time;
+        float last_anglex, last_angleY;
+        
+        // Auxiliary functions
+        Vector3D<float> getGyroOffset();
+        void printValues(const char* label, float x, float y, float z);
+        void computeAngle_ComplementaryFilter(float alpha);
+        void computeAccelOrientation();
+        int sign(int value);
+    public:
+        void initialize();
+        void calibrate();
+        void update();
+        void print();
+        void printSensorData();
+
+};
